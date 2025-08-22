69f7c8957c1c721ab05a848b2e60921d5e887569
swordrakhi <k1516791@students.katyisd.org>
2025-08-22 14:14:46 +0000

    Setting up main

--- first-react/src/main.jsx
+++ first-react/src/main.jsx
@@ -1,10 +1,3 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+root.render(<h1>This is React!</h1>)
