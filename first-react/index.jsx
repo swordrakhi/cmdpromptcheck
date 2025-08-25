91de802b2d19e3cc6ae0b1a1bca1515b57c6f941
swordrakhi <k1516791@students.katyisd.org>
2025-08-25 14:55:11 +0000

    React facts project

--- first-react/index.jsx
+++
@@ -1,3 +0,0 @@
-import { createRoot } from "react-dom/client"
-const root = createRoot(document.getElementById("root"));
-root.render(<h1>Gh</h1>)
---
+++ first-react/src/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+  return(
+    <header>
+        <nav>
+<img src="src/react.png" alt="" />
+        <span>ReactFacts</span>
+        </nav>
+    </header>
+  )
+}
